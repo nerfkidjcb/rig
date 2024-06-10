c33480746deaf46e042c674439eb4eba5b7d31f8
Dan Smith <dev@dansmith.tech>
2024-06-11 00:58:35 +0100

    Fix dwmblocks sizing changing status bar width

--- dwm/blocks.def.h
+++ dwm/blocks.def.h
@@ -1,17 +1,16 @@
 #include <stdlib.h>
 
 static const Block blocks[] = {
-    /*Icon*/	     /*Command*/                                               /*Update Interval*/   /*Update Signal*/
-
-    {"  ",  	     "top -bn1 -n1 | grep '%Cpu(s)' | awk '{print $2+$4+$6\"%  \"}'",      		         60,                 0},
-    {"",	     "sensors | grep 'Tctl' | awk '{print substr($2, 2)}'",                      		 60,                 0}, // PC
-    {"",	     "sensors | grep 'Package id 0' | awk '{print substr($4, 2)}'",                      		 60,                 0}, // Laptop
-    {"      ",      "nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | sed 's/ //g'",              60,                 0}, // PC
-    {"  ",	     "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | awk '{print $1 \"°C\"}'", 	 60,                 0}, // PC
-    {"      ",      "top -bn1 -n1 | grep 'MiB Mem' | awk '{printf \"%.1fGB\\n\", \$8 / 1024\"GB\"}'",	         60,                 0},
-    {"      ",    	"echo $(cat /sys/class/power_supply/BAT0/capacity)%",                    		  60,                 0}, // Laptop
-    {"    󰥔  ",	     "date '+%H:%M %-d %B %Y'",                                          	                 60,                 0},
+   /* icon, command, interval, signal */
+   {"  ", "top -bn1 | grep '%Cpu(s)' | awk '{printf \"%02d%%\", int($2+$4+$6+0.5)}'", 60, 0},
+   {"  ", "sensors | grep 'Tctl' | awk '{temp = int(substr($2, 2) + 0.5); printf \"%02d°C\", temp}'", 60, 0}, // pc
+   {"  ", "sensors | grep 'Package id 0' | awk '{print int(substr($4, 2) + 0.5) \"°C\"}'", 60, 0}, // laptop
+   {"      ", "nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | awk '{printf \"%02d\", $1}'", 60, 0}, // pc
+   {"  ", "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | awk '{print sprintf(\"%02.0f°C\", $1)}'", 60, 0}, // pc
+   {"      ", "top -bn1 | grep 'MiB Mem' | awk '{used=$8; total=$4; printf \"%02d%%\", int((used/total)*100 + 0.5)}'", 60, 0},
+   {"      ", "printf \"%02d%%\" $(cat /sys/class/power_supply/BAT0/capacity)", 60, 0}, // laptop
+   {"    󰥔  ", "date '+%H:%M %-d %B %Y'", 60, 0},
 };
 
-static char delim[] = "\0";
+static char delim[] = "";
 static unsigned int delimLen = 0;
