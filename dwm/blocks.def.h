370db227473740e2aab2d8c072cf0517880fbf04
Dan Smith <dev@dansmith.tech>
2024-06-10 23:32:03 +0100

    Add battery level back to dwmblocks for laptop

--- dwm/blocks.def.h
+++ dwm/blocks.def.h
@@ -8,6 +8,7 @@ static const Block blocks[] = {
     {"      ",      "nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | sed 's/ //g'",              60,                 0}, // PC
     {"  ",	     "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | awk '{print $1 \"°C\"}'", 	 60,                 0}, // PC
     {"      ",      "top -bn1 -n1 | grep 'MiB Mem' | awk '{printf \"%.1fGB\\n\", \$8 / 1024\"GB\"}'",	         60,                 0},
+    {"      ",    	"echo $(cat /sys/class/power_supply/BAT0/capacity)%",                    		  60,                 0}, // Laptop
     {"    󰥔  ",	     "date '+%H:%M %-d %B %Y'",                                          	                 60,                 0},
 };
 
