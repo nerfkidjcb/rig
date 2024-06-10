8f95b7a97d7ea194e027a890ad8153bd85427027
Dan Smith <dev@dansmith.tech>
2024-06-10 23:35:29 +0100

    Add CPU temperature reading back to dwmblocks for laptop

--- dwm/blocks.def.h
+++ dwm/blocks.def.h
@@ -5,6 +5,7 @@ static const Block blocks[] = {
 
     {"  ",  	     "top -bn1 -n1 | grep '%Cpu(s)' | awk '{print $2+$4+$6\"%  \"}'",      		         60,                 0},
     {"",	     "sensors | grep 'Tctl' | awk '{print substr($2, 2)}'",                      		 60,                 0}, // PC
+    {"",	     "sensors | grep 'Package id 0' | awk '{print substr($4, 2)}'",                      		 60,                 0}, // Laptop
     {"      ",      "nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader | sed 's/ //g'",              60,                 0}, // PC
     {"  ",	     "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader | awk '{print $1 \"°C\"}'", 	 60,                 0}, // PC
     {"      ",      "top -bn1 -n1 | grep 'MiB Mem' | awk '{printf \"%.1fGB\\n\", \$8 / 1024\"GB\"}'",	         60,                 0},
