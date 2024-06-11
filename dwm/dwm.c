fff9ea68d9323a25c7d57a6efa80cd13054496e6
Dan Smith <dev@dansmith.tech>
2024-06-11 01:23:07 +0100

    Fix empty title redraw not being full available width

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -750,7 +750,7 @@ drawbar(Monitor *m)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
-			drw_rect(drw, x, 0, w - lrpad * sp, bh, 1, 1);
+			drw_rect(drw, x, 0, w - lrpad, bh, 1, 1);
 		}
 	}
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
