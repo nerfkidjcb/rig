a83d50a1d0b51d38df15fb31b159d4f44a8fe007
Dan Smith <dev@dansmith.tech>
2024-09-01 19:53:02 +0200

    Move icon dependency into theme import

--- stow/.config/nvim/lua/plugins/theme.lua
+++ stow/.config/nvim/lua/plugins/theme.lua
@@ -1,8 +1,13 @@
 return {
-   "projekt0n/github-nvim-theme",
-   lazy = false,
-   priority = 1000,
-   config = function()
-      vim.cmd("colorscheme github_dark_default")
-   end,
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme github_dark_default")
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
 }
