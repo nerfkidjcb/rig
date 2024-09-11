af5f8aa957b8ea8b95f769aaa54c28be0dc6cab3
Dan Smith <daniel.james.smith@cern.ch>
2024-09-11 09:22:12 +0200

    Fix mappings for code actions/definitions/etc and format lsp plugin

--- stow/.config/nvim/lua/plugins/lsp.lua
+++ stow/.config/nvim/lua/plugins/lsp.lua
@@ -1,151 +1,151 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					-- Python
-					"pyright",
+   {
+      "williamboman/mason.nvim",
+      config = function()
+         require("mason").setup()
+      end,
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+         require("mason-lspconfig").setup({
+            ensure_installed = {
+               -- Python
+               "pyright",
 
-					-- C/C++
-					"clangd",
+               -- C/C++
+               "clangd",
 
-					-- Web
-					"html",
-					"cssls",
-					"tsserver",
-					"eslint",
+               -- Web
+               "html",
+               "cssls",
+               "tsserver",
+               "eslint",
 
-					-- Lua
-					"lua_ls",
+               -- Lua
+               "lua_ls",
 
-					-- JSON
-					"jsonls",
+               -- JSON
+               "jsonls",
 
-					-- YAML
-					"yamlls",
+               -- YAML
+               "yamlls",
 
-					-- Rust / TOML
-					"taplo",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+               -- Rust / TOML
+               "taplo",
+            },
+         })
+      end,
+   },
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         local lspconfig = require("lspconfig")
+         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Python
-			lspconfig.pyright.setup({ capabilities = capabilities })
+         -- Python
+         lspconfig.pyright.setup({ capabilities = capabilities })
 
-			-- C/C++
-			lspconfig.clangd.setup({ capabilities = capabilities })
+         -- C/C++
+         lspconfig.clangd.setup({ capabilities = capabilities })
 
-			-- Web
-			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.cssls.setup({ capabilities = capabilities })
-			lspconfig.tsserver.setup({ capabilities = capabilities })
-			lspconfig.eslint.setup({ capabilities = capabilities })
+         -- Web
+         lspconfig.html.setup({ capabilities = capabilities })
+         lspconfig.cssls.setup({ capabilities = capabilities })
+         lspconfig.tsserver.setup({ capabilities = capabilities })
+         lspconfig.eslint.setup({ capabilities = capabilities })
 
-			-- Lua
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
+         -- Lua
+         lspconfig.lua_ls.setup({ capabilities = capabilities })
 
-			-- JSON
-			lspconfig.jsonls.setup({ capabilities = capabilities })
+         -- JSON
+         lspconfig.jsonls.setup({ capabilities = capabilities })
 
-			-- YAML
-			lspconfig.yamlls.setup({ capabilities = capabilities })
+         -- YAML
+         lspconfig.yamlls.setup({ capabilities = capabilities })
 
-			-- Rust / TOML
-			lspconfig.taplo.setup({ capabilities = capabilities })
+         -- Rust / TOML
+         lspconfig.taplo.setup({ capabilities = capabilities })
 
-			vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		config = function()
-			require("mason-null-ls").setup({
-				ensure_installed = {
-					-- Python
-					"pylint",
-					"mypy",
-					"black",
-					"isort",
+         vim.keymap.set("n", "<C-w>", vim.lsp.buf.hover, {})
+         vim.keymap.set("n", "<C-a>", vim.lsp.buf.code_action, {})
+         vim.keymap.set("n", "<C-s>", vim.lsp.buf.definition, {})
+      end,
+   },
+   {
+      "jay-babu/mason-null-ls.nvim",
+      config = function()
+         require("mason-null-ls").setup({
+            ensure_installed = {
+               -- Python
+               "pylint",
+               "mypy",
+               "black",
+               "isort",
 
-					-- C/C++
-					"clang-format",
+               -- C/C++
+               "clang-format",
 
-					-- Web
-					"eslint_d",
+               -- Web
+               "eslint_d",
 
-					-- Lua
-					"stylua",
+               -- Lua
+               "stylua",
 
-					-- JSON
-					"prettier",
+               -- JSON
+               "prettier",
 
-					-- YAML
-					"yamllint",
+               -- YAML
+               "yamllint",
 
-					-- Web / Markdown / JSON / YAML
-					"prettier",
-				},
-			})
-		end,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = {
-			"nvimtools/none-ls-extras.nvim",
-		},
-		config = function()
-			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+               -- Web / Markdown / JSON / YAML
+               "prettier",
+            },
+         })
+      end,
+   },
+   {
+      "nvimtools/none-ls.nvim",
+      dependencies = {
+         "nvimtools/none-ls-extras.nvim",
+      },
+      config = function()
+         local null_ls = require("null-ls")
+         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-			null_ls.setup({
-				sources = {
-					-- Python
-					null_ls.builtins.diagnostics.pylint,
-					null_ls.builtins.diagnostics.mypy,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
+         null_ls.setup({
+            sources = {
+               -- Python
+               null_ls.builtins.diagnostics.pylint,
+               null_ls.builtins.diagnostics.mypy,
+               null_ls.builtins.formatting.black,
+               null_ls.builtins.formatting.isort,
 
-					-- C/C++
-					null_ls.builtins.formatting.clang_format,
+               -- C/C++
+               null_ls.builtins.formatting.clang_format,
 
-					-- Lua
-					null_ls.builtins.formatting.stylua,
+               -- Lua
+               null_ls.builtins.formatting.stylua,
 
-					-- YAML
-					null_ls.builtins.diagnostics.yamllint,
+               -- YAML
+               null_ls.builtins.diagnostics.yamllint,
 
-					-- Web / Markdown / JSON / YAML
-					null_ls.builtins.formatting.prettier,
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format()
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
+               -- Web / Markdown / JSON / YAML
+               null_ls.builtins.formatting.prettier,
+            },
+            on_attach = function(client, bufnr)
+               if client.supports_method("textDocument/formatting") then
+                  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                  vim.api.nvim_create_autocmd("BufWritePre", {
+                     group = augroup,
+                     buffer = bufnr,
+                     callback = function()
+                        vim.lsp.buf.format()
+                     end,
+                  })
+               end
+            end,
+         })
+      end,
+   },
 }
